099d09ab3d32bbc5d7615d906879094f82ae2006
Martin <tenzin.braun@gmail.com>
2019-11-05 17:13:55 +0100

    [BaseProject update]

---
+++ Pokemon Cards Exchange/app/src/main/java/fr/iutbourg/pokemoncardsexchange/activity/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package fr.iutbourg.pokemoncardsexchange.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.iutbourg.pokemoncardsexchange.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
